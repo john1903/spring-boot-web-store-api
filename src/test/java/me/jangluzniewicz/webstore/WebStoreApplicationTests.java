1492a8e5f9e80ce07711e55bf137f03bdca1a7df
Jan Gluźniewicz <Jan.gluzniewicz@gmail.com>
2025-01-02 19:44:46 +0100

    project setup

---
+++ src/test/java/me/jangluzniewicz/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jangluzniewicz.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

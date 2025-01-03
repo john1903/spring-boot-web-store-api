93ed875466d5bd3c401c7e8f3eb5ee2cdc9f5750
Jan Gluźniewicz <Jan.gluzniewicz@gmail.com>
2025-01-03 12:53:39 +0100

    @AllArgsConstructor @RequiredArgsConstructor lombok annotation added to entities

--- src/main/java/me/jangluzniewicz/webstore/categories/entities/CategoryEntity.java
+++ src/main/java/me/jangluzniewicz/webstore/categories/entities/CategoryEntity.java
@@ -1,27 +1,17 @@
 package me.jangluzniewicz.webstore.categories.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
-@NoArgsConstructor @Getter @Setter @ToString
+@AllArgsConstructor @RequiredArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@ToString
 @Table(name = "categories")
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = true)
-    private String name;
-
-    public CategoryEntity(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public CategoryEntity(String name) {
-        this.name = name;
-    }
+    @NonNull private String name;
 }
--- src/main/java/me/jangluzniewicz/webstore/order_statuses/entities/OrderStatusEntity.java
+++ src/main/java/me/jangluzniewicz/webstore/order_statuses/entities/OrderStatusEntity.java
@@ -1,27 +1,17 @@
 package me.jangluzniewicz.webstore.order_statuses.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
-@NoArgsConstructor @Getter @Setter @ToString
+@AllArgsConstructor @RequiredArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@ToString
 @Table(name = "order_statuses")
 public class OrderStatusEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = true)
-    private String name;
-
-    public OrderStatusEntity(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public OrderStatusEntity(String name) {
-        this.name = name;
-    }
+    @NonNull private String name;
 }

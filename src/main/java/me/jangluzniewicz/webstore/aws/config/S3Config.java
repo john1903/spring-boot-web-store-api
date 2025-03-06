594da1cd80d62ee141af191d0b96b02c8d542d96
johny1903 <jan.gluzniewicz@gmail.com>
2025-03-06 12:49:31 +0100

    Add AWS URL and region configuration: update main.yaml, application-ci.yaml, application-prod.yaml, and application-local.yaml.example to include AWS_URL and AWS_REGION; modify S3Config to utilize these new properties for S3 client configuration.

--- src/main/java/me/jangluzniewicz/webstore/aws/config/S3Config.java
+++ src/main/java/me/jangluzniewicz/webstore/aws/config/S3Config.java
@@ -1,5 +1,6 @@
 package me.jangluzniewicz.webstore.aws.config;
 
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,18 +14,25 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 public class S3Config {
   private final String accessKey;
   private final String secretKey;
-  private final Region region = Region.EU_CENTRAL_1;
+  private final String url;
+  private final Region region;
 
   public S3Config(
-      @Value("${aws.access-key}") String accessKey, @Value("${aws.secret-key}") String secretKey) {
+      @Value("${aws.access-key}") String accessKey,
+      @Value("${aws.secret-key}") String secretKey,
+      @Value("${aws.url}") String url,
+      @Value("${aws.region}") String region) {
     this.accessKey = accessKey;
     this.secretKey = secretKey;
+    this.url = url;
+    this.region = Region.of(region);
   }
 
   @Bean
   public S3Client s3Client() {
     AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
     return S3Client.builder()
+        .endpointOverride(URI.create(url))
         .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
         .region(region)
         .build();
@@ -34,6 +42,7 @@ public class S3Config {
   public S3Presigner s3Presigner() {
     AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
     return S3Presigner.builder()
+        .endpointOverride(URI.create(url))
         .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
         .region(region)
         .build();
